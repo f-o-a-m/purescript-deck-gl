0df3f2b61c926f087ec1a15decc095127f158a5f
blinky3713 <blinky@foam.space>
2018-03-12 14:03:20 -0400

    compileS

--- examples/icon/src/Main.purs
+++ examples/icon/src/Main.purs
@@ -1,7 +1,8 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff.Console
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
 
 main :: Eff (console :: CONSOLE) Unit
 main = log "Example!"
