1fe142ead15ed7fb800f93628649f9bb9bb0d2bf
blinky3713 <blinky@foam.space>
2018-03-12 19:03:59 -0400

    fix deps

--- examples/icon/src/Config.purs
+++
@@ -1,8 +0,0 @@
-module Config where
-
-mapStyle :: String
-mapStyle = "mapbox://styles/mapbox/dark-v9"
-
-mapboxApiAccessToken :: String
-mapboxApiAccessToken = "pk.eyJ1Ijoia2VqYWNlIiwiYSI6ImNqMWIxYnc2MzA5aGYycW1va2pmN3pzcXgifQ.sjdeDT6rkYOBQxCnoYLssw"
-
