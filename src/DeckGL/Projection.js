64c3f2b6c10465ff85e601f063c8b95d781458d3
blinky3713 <blinky@foam.space>
2018-03-13 22:23:22 -0400

    proper curry

--- src/DeckGL/Projection.js
+++ src/DeckGL/Projection.js
@@ -5,13 +5,17 @@ exports.makeMercatorProjector = function (vp) {
     return mp;
 };
 
-exports.unproject = function (projector, screenCoords) {
-    return projector.unproject([screenCoords.x, screenCoords.y]);
+exports.unproject = function (projector) {
+    return function(screenCoords) {
+        return projector.unproject([screenCoords.x, screenCoords.y]);
+    };
 };
 
-exports.project = function (projector, lngLat) {
-    console.log(projector);
-    console.log(lngLat);
-    const xy = projector.project(lngLat);
-    return {x: xy[0], y: xy[1]};
+exports.project = function (projector) {
+    return function(lngLat) {
+        console.log(projector);
+        console.log(lngLat);
+        const xy = projector.project(lngLat);
+        return {x: xy[0], y: xy[1]};
+    };
 };
