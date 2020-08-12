9f7ea0eb1a7863b91952b46ce1d0c4f46152e31c
blinky3713 <martin@foam.space>
2020-08-12 12:09:56 -0700

    hexagon layer

--- src/DeckGL/Layer/Hexagon.purs
+++ src/DeckGL/Layer/Hexagon.purs
@@ -1,21 +1,19 @@
 module DeckGL.Layer.Hexagon where
 
 import DeckGL (Layer)
-import DeckGL.BaseProps (BaseProps, LightSettings)
+import DeckGL.BaseProps (BaseProps)
 import WebMercator.LngLat (LngLat)
 
-foreign import defaultHexagonProps :: HexagonLayerProps
+foreign import defaultHexagonProps :: forall d. HexagonLayerProps d
+ 
+foreign import makeHexagonLayer :: forall d. HexagonLayerProps d -> Layer
 
-foreign import makeHexagonLayer :: HexagonLayerProps -> Layer
-
-type HexData =
-  { position :: Array LngLat
-  }
+type HexData d = {|d}
 
 -- | - `coverage`: The final radius of hexagon is calculated by coverage * radius. Note:
 -- | coverage does not affect how points are binned. The radius of the bin is determined
 -- | only by the radius property.
--- | - `colorRange`: A tuple of numbers indicating the min and max range of elevation.
+-- | - `elevationRange`: A tuple of numbers indicating the min and max range of elevation.
 -- | - `elevationScale`: Hexagon elevation multiplier. The actual elevation is calculated
 -- | by elevationScale * getElevation(d). elevationScale is a handy property to scale all
 -- | hexagons without updating the data.
@@ -23,14 +21,17 @@ type HexData =
 -- | in each cell. If set to false, all cells will be flat.
 -- | `radius`: Radius of hexagons measured in meters.
 
-type HexagonLayerProps = BaseProps
-  ( coverage :: Int
-  , colorRange :: Array (Array Int)
+type HexagonLayerProps d = BaseProps
+  ( getPosition :: HexData d -> LngLat
+  , lowerPercentile :: Number
+  , upperPercentile :: Number
+  , elevationLowerPercentile :: Number
+  , elevationUpperPercentile :: Number
   , elevationRange :: Array Int
   , elevationScale :: Int
+  , coverage :: Int
+  , colorRange :: Array (Array Int)
   , extruded :: Boolean
-  , lightSettings :: LightSettings
   , radius :: Int
-  , upperPercentile :: Int
   )
-  HexData
+  (HexData d)
