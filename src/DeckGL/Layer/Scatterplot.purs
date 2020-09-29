97f956b293229eff0b7d3cec49b18e07ecc8e684
Serhii Khoma <srghma@gmail.com>
2020-09-29 15:19:50 +0300

    feat: Upgrading https://deck.gl/docs/upgrade-guide#upgrading-from-deckgl-v63-to-v64

--- src/DeckGL/Layer/Scatterplot.purs
+++ src/DeckGL/Layer/Scatterplot.purs
@@ -7,14 +7,15 @@ import WebMercator.LngLat (LngLat)
 foreign import defaultScatterplotProps :: ScatterplotLayerProps
 foreign import makeScatterplotLayer :: ScatterplotLayerProps -> Layer
 
+-- https://deck.gl/docs/api-reference/layers/scatterplot-layer
 type ScatterplotData =
   { position :: LngLat
   , radius :: Number
   , color :: Array Int
   }
 
--- | - `outline`: Only draw the outline of the points
+-- | - `stroked`: Only draw the stroked of the points
 type ScatterplotLayerProps = BaseProps
-  ( outline :: Boolean
+  ( stroked :: Boolean
   )
   ScatterplotData
